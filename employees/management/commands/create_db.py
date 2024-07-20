943112eaf1e65a7629d408601436b7998be86246
gorestea <rojofamily@yandex.ru>
2024-07-20 21:27:31 +0300

    change language

--- employees/management/commands/create_db.py
+++ employees/management/commands/create_db.py
@@ -7,21 +7,19 @@ class Command(BaseCommand):
     help = 'Заполнение базы данных начальными данными'
 
     def handle(self, *args, **kwargs):
-        # Создание 25 подразделений с иерархией до 5 уровней
         departments = []
         for i in range(1, 26):
             parent = random.choice(departments) if departments else None
-            department = Department.objects.create(name=f'Department {i}', parent=parent)
+            department = Department.objects.create(name=f'Отдел №{i}', parent=parent)
             departments.append(department)
 
-        # Создание 50,000 сотрудников
-        positions = ['Manager', 'Developer', 'Designer', 'Tester', 'Analyst']
+        positions = ['Менеджер', 'Разработчик', 'Дизайнер', 'Тестировщик', 'Аналитик']
         for i in range(50000):
             department = random.choice(departments)
             hire_date = date.today() - timedelta(days=random.randint(0, 3650))
             salary = round(random.uniform(30000, 150000), 2)
             Employee.objects.create(
-                full_name=f'Employee {i + 1}',
+                full_name=f'Сотрудник {i + 1}',
                 position=random.choice(positions),
                 hire_date=hire_date,
                 salary=salary,
